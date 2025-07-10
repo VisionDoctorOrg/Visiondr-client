8b110fc8d4b32fb1ec768f1951a4b536f8865dac
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2025-07-10 04:58:08 +0100

    Feat: depl

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import path from "path"
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
+  base: "./", // 👈 VERY IMPORTANT
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
