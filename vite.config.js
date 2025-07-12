1bee8f89c5729469a847a349a035316ed1e68b78
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2025-07-12 08:39:21 +0100

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,37 @@
-// import path from "path"
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+// // import path from "path"
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
+// // // https://vitejs.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// //   resolve: {
+// //     alias: {
+// //       "@": path.resolve(__dirname, "./src"),
+// //     },
+// //   },
+// // })
+
+
+// import path from "path";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
 // export default defineConfig({
+//   base: "./", // 👈 VERY IMPORTANT
 //   plugins: [react()],
 //   resolve: {
 //     alias: {
 //       "@": path.resolve(__dirname, "./src"),
 //     },
 //   },
-// })
+// });
 
 
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "./", // 👈 VERY IMPORTANT
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+export default {
+  base: './',  // Relative paths for static deployment
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+}
